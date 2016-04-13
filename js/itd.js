50a22364761f9f67b3a6e31206722a67f089dbec
Mohammadreza <zarei.mohamadreza@gmail.com>
2016-04-13 16:48:48 +0200

    add js file

---
+++ js/itd.js
@@ -0,0 +1,10 @@
+/**
+ * Created by mohammadReza on 4/13/2016.
+ */
+(function($){
+    Drupal.behaviors.idt={
+        attach:function(){
+            console.log("idt.js");
+        }
+    }
+})(jQuery);
